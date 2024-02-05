dacb10fe7182ddafad34c5978b97a243b4a69e45
Farkas08 <bercif.android@gmail.com>
2024-02-05 19:23:34 +0100

    addplanet and get planet function commit

--- Lesson1/src/first/World.java
+++ Lesson1/src/first/World.java
@@ -1,13 +1,31 @@
 package first;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
     private String name;
-    Planet planet = new Planet("Earth",1,1);
+    // list for the planets
+    private List<Planet> planets;
+
     public World(String name) {
-    	this.name = name;
+        this.name = name;
+        planets = new ArrayList<>();
+    }
+
+    // add planets to the world
+    public void addPlanet(Planet p) {
+        planets.add(p);
+    }
+
+    // get all the planets of the world
+    public void getPlanets() {
+        for (Planet planet : planets) {
+            System.err.println(planet);
+        }
     }
 
     public String toString() {
-    	return "Worlds name:"+name+", "+planet;
-    } 
+        return "Worlds name:" + name;
+    }
 }
--- Lesson1/src/first/worldCreator.java
+++ Lesson1/src/first/worldCreator.java
@@ -5,6 +5,9 @@ public class worldCreator {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		World world = new World("Solar system");
+		Planet planet = new Planet("Earth", 1, 1);
+		world.addPlanet(planet);
 		System.out.println(world);
+		world.getPlanets();
 	}
 }
